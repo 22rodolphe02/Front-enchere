ec0389687930bd696506d9a65cd01091e9db2a36
22rodolphe02 <tsiafararodolphe@gmail.com>
2023-02-03 21:36:37 +0300

    build

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,2 +1,3 @@
 export const SPRING_URL = "https://venteenchere-cloud-back-spring.up.railway.app/"
-// export const SPRING_URL = "http://localhost:8080/"
+
+// export const SPRING_URL = "http://localhost:8080//"
